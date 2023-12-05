8ebed7304dd6c761f6952de0ddf4b0abc47c973a
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2023-12-05 05:54:19 -0400

    adicionada img no card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,13 @@
 interface CardProps {
   nome: string;
+  imagem: string
 }
 
-export const Card = ({ nome }: CardProps) => {
+export const Card = ({ nome, imagem }: CardProps) => {
   return (
     <li>
       <div>
+        <img src={imagem} alt={nome}/>
         <h2>{nome}</h2>
       </div>
     </li>
